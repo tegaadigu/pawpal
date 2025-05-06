ad3df51fbad1d6cc5760f42888a36504944dd094
Tega <tega.adigu@lurn.com>
2025-05-06 01:09:48 -0400

    chore: code cleanup

--- web/packages/pawpal-components/eslint.config.js
+++ web/packages/pawpal-components/eslint.config.js
@@ -4,6 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
+// @todo Tega | Removed build job as this component uses vite and its meant to be just a component library
+// please remove vite and eventually hook it up to story book to serve as a full component library
+
+
 export default tseslint.config(
   { ignores: ['dist'] },
   {
